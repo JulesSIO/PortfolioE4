0517e148547410b70da05e90195c8137ef229486
QUENTIN Jules <Jules.QUENTIN@cnfpt.fr>
2025-03-12 14:45:59 +0100

    Modifs tasks + MAJ CV + Test certi

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const title=document.getElementsByClassName('tasks-title');
-    const tasks1=document.getElementsByClassName('tasks1');
-
-    title.addEventListener('click', ()=>{
-        tasks1.style.display='flex';
-    });
-
+    alert("Bienvenue sur mon portfolio")
 });
